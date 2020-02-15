bd08106a0c7d65f36be6f2fcc5500bf85157dd08
Maxime Smolis <mpsmolis@gmail.com>
2020-02-15 15:53:22 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log(document.querySelector("body > div > h1"));
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
