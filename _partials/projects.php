bb7dcc211afcc54b73308c999c33588c9c706167
Maxime Smolis <mpsmolis@gmail.com>
2020-03-04 18:28:49 +0100

    1 project file added

--- _partials/projects.php
+++ _partials/projects.php
@@ -5,7 +5,7 @@
     <div class="col s12 m4">
       <div class="card">
         <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="public/images/festroad.png">
+          <img class="activator" src="public/images/projects/festroad.png">
         </div>
         <div class="card-reveal">
           <span class="card-title grey-text center-align"><i class="material-icons grey-text right">close</i></span>
@@ -19,7 +19,7 @@
     <div class="col s12 m4">
       <div class="card">
         <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="public/images/festroad.png">
+          <img class="activator" src="public/images/projects/chezriz.png">
         </div>
         <div class="card-reveal">
           <span class="card-title grey-text center-align"><i class="material-icons grey-text right">close</i></span>
@@ -33,7 +33,7 @@
     <div class="col s12 m4">
       <div class="card">
         <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="public/images/festroad.png">
+          <img class="activator" src="public/images/projects/festroad.png">
         </div>
         <div class="card-reveal">
           <span class="card-title grey-text center-align"><i class="material-icons grey-text right">close</i></span>
